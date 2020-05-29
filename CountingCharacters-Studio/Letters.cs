c32ae6aecb4b77cbf55c0aa8ba322810823c6dfc
derickard <derickard@gmail.com>
2020-05-29 13:06:30 -0400

    Studio finished

---
+++ CountingCharacters-Studio/Letters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountingCharacters_Studio
+{
+    class Letters
+    {
+        private Dictionary<char, int> count = new Dictionary<char, int>();
+        private char[] charArr;
+
+        public Letters(string str)
+        {
+            this.charArr = str.ToCharArray();
+            this.count = Count();
+        }
+
+        // Get count of letters
+        private Dictionary<char, int> Count()
+        {
+            foreach(char c in this.charArr)
+            {
+                if(Char.IsLetter(c))
+                {
+                    if(this.count.ContainsKey(c))
+                    {
+                        this.count[c] += 1;
+                    } else
+                    {
+                        this.count[c] = 1;
+                    }
+                }
+            }
+            return count;
+        }
+
+        // Display count of letters
+        public void Print()
+        {
+            foreach (KeyValuePair<char, int> l in this.count)
+            {
+                Console.WriteLine($"{l.Key}\t{l.Value}");
+            }
+        }
+
+    }
+}
---
+++ CountingCharacters-Studio/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CountingCharacters_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask for Case Sensitive
+            Console.Write("Case-Sensitive? (y/N): ");
+            string input = Console.ReadLine();
+            bool caseSense = false;
+            if(Equals(input, "Y") || Equals(input, "y") || Equals(input, "Yes") || Equals(input, "yes"))
+            {
+                caseSense = true;
+            }
+
+            // Ask if file to read in 
+            Console.Write("Do you have a file? (Y/n): ");
+            input = Console.ReadLine();
+            string myString;
+            if(Equals(input, "Y") || Equals(input, "y") || Equals(input, "Yes") || Equals(input, "yes") || Equals(input, ""))
+            {
+                Console.WriteLine("Enter full path to file: ");
+                string textFilePath = Console.ReadLine();
+                myString = File.ReadAllText(textFilePath);
+                if (!caseSense)
+                {
+                    myString = myString.ToLower();
+                }
+            } else
+            {
+                Console.WriteLine("Enter text to parse:");
+                myString = Console.ReadLine();
+                if (!caseSense)
+                {
+                    myString = myString.ToLower();
+                }
+            }
+
+            // Get character counts
+            Console.WriteLine("Calculating character count...");
+            Letters myLetters = new Letters(myString);
+            myLetters.Print();
+        }
+    }
+}
